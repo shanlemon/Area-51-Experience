ef4ceb6d1cc3bf130b345f53dc933a287b53094c
Shan Memon <shankmemon@gmail.com>
2019-09-20 23:05:11 -0500

    Started project, Can move

---
+++ Homebound/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+	[SerializeField]
+	private int movementSpeed = 5;
+
+	[SerializeField]
+	private GameObject cameraHeadObject;
+
+
+	public VRTK_ControllerEvents controllerEvents;
+
+
+
+	// Start is called before the first frame update
+	void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		Vector3 moveAmount = Time.deltaTime * ((controllerEvents.GetTouchpadAxis().x * movementSpeed * cameraHeadObject.transform.right) + (controllerEvents.GetTouchpadAxis().y * movementSpeed * cameraHeadObject.transform.forward));
+		moveAmount.y = 0;
+		transform.position += moveAmount;//Debug.Log();
+	}
+}
